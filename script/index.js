3e101b63e545b5937c79b41649a33bcbd3057a7a
Syed Qaswar <qaswarbukhari5@gmail.com>
2026-04-10 20:56:16 +0500

    js code

--- script/index.js
+++ script/index.js
@@ -1,86 +1,173 @@
-// runs inside the browser
+// // runs inside the browser
 
-// variables and data type
-let name = 'Saad'; //string
-let age = 20; //integer
-let city = 'Lahore';
-let marks = 90.7; //float
-const cnic = '123-4567-8'; //string
-let status = false; //boolean
+// // variables and data type
+// let name = 'Saad'; //string
+// let age = 20; //integer
+// let city = 'Lahore';
+// let marks = 90.7; //float
+// const cnic = '123-4567-8'; //string
+// let status = false; //boolean
 
-// output
-console.log(name)
-console.log(age)
-console.log(city)
-console.log(status)
+// // output
+// console.log(name)
+// console.log(age)
+// console.log(city)
+// console.log(status)
 
-// operators: arithmetic, assignment, comparison
+// // operators: arithmetic, assignment, comparison
 
-// function
+// // function
 
-// defining a function
-function greet(name){ //parameter
-    // function block
-    // console.log('Hello, Guest')
-    console.log('Hello,' + name)
-}
-
-// calling a function
-greet('Abdullah') //argument
-
-
-// HTML DOM
-
-// writing text using JS
-// document.getElementById('text').innerText = 'Hello JS';
-// document.querySelector('h1').innerText = 'Hello JS';
-
-// styling with JS
-// function changeColor(){
-//     document.querySelector('h1').style.color = 'red';
+// // defining a function
+// function greet(name){ //parameter
+//     // function block
+//     // console.log('Hello, Guest')
+//     console.log('Hello,' + name)
 // }
 
-// using event listeners
+// // calling a function
+// greet('Abdullah') //argument
 
-// with a named function
-// document.getElementById('btn').addEventListener('click', changeColor)
 
-// with an anonymous function
-// document.getElementById('btn').addEventListener('click', function(){
-//     document.querySelector('h1').style.color = 'red';
-// })
+// // HTML DOM
 
-// adding multiple styles 
+// // writing text using JS
+// // document.getElementById('text').innerText = 'Hello JS';
+// // document.querySelector('h1').innerText = 'Hello JS';
 
-// let box = document.getElementById('box')
+// // styling with JS
+// // function changeColor(){
+// //     document.querySelector('h1').style.color = 'red';
+// // }
 
-// document.getElementById('btn').addEventListener('click', function(){
-//     box.style.backgroundColor = 'aqua'
-//     box.style.color = 'white'
-//     box.style.border = '2px solid red'
-//     box.style.padding = '10px'
-// })
+// // using event listeners
+
+// // with a named function
+// // document.getElementById('btn').addEventListener('click', changeColor)
+
+// // with an anonymous function
+// // document.getElementById('btn').addEventListener('click', function(){
+// //     document.querySelector('h1').style.color = 'red';
+// // })
+
+// // adding multiple styles 
+
+// // let box = document.getElementById('box')
+
+// // document.getElementById('btn').addEventListener('click', function(){
+// //     box.style.backgroundColor = 'aqua'
+// //     box.style.color = 'white'
+// //     box.style.border = '2px solid red'
+// //     box.style.padding = '10px'
+// // })
+
+
+// // let btn = document.getElementById('btn')
+// // let input = document.getElementById('input')
+// // let txt = document.getElementById('text')
+
+// // btn.addEventListener('click', function(){
+// //     let val = input.value
+// //     if(val.length >= 5){
+// //         txt.innerText = val
+// //     } else{
+// //         txt.innerText = 'Number of chars should be equals to 5 or greater than 5'
+// //     }
+// // })
+
+
+// // changing color with user input
+// // let btn = document.getElementById('btn')
+// // let txt = document.getElementById('text')
+
+// // btn.addEventListener('click', function(){
+// //     let color = document.getElementById('color').value
+// //     txt.style.color = color
+// // })
+
+// // regular normal
+// function addValues(a, b){
+//     console.log(a + b)
+// }
+// addValues(5, 10)
+
+// // arrow functions (anonymous, inline)
+// let sum = (a , b) => {
+//     console.log(a + b)
+// }
+// sum(2, 4)
 
 
 // let btn = document.getElementById('btn')
-// let input = document.getElementById('input')
 // let txt = document.getElementById('text')
 
-// btn.addEventListener('click', function(){
-//     let val = input.value
-//     if(val.length >= 5){
-//         txt.innerText = val
-//     } else{
-//         txt.innerText = 'Number of chars should be equals to 5 or greater than 5'
-//     }
+// btn.addEventListener('click', () =>{
+//     let color = document.getElementById('color').value
+//     txt.style.color = color
 // })
 
 
-// changing color with user input
-let btn = document.getElementById('btn')
-let txt = document.getElementById('text')
+// // arrays
+// // let city = 'Lahore'
+// let cities = ['Lahore', 'Karachi', 'Quetta']
+// console.log(cities)
 
-btn.addEventListener('click', function(){
-    let color = document.getElementById('color').value
-    txt.style.color = color
-})
+// // accessing a single value with indexing
+// console.log(cities[0])
+// let x = cities[1]
+// console.log(x)
+
+// cities[2] = 'Multan' //assigning a new value to index 2
+// cities[3] = 'Quetta' //creates a new index
+// console.log(cities)
+
+// // methods and properties
+// cities.length //property
+// cities.push('Sialkot') //method
+// let val = cities.pop() //removes and returns the value
+// console.log(cities)
+// console.log(val) //returned value
+
+
+// // loops
+
+// // for loop
+// for(let i = 0; i < 10; i++){
+//     console.log(i)
+// }
+
+// // multiplication table
+// let result
+// for(let i = 1; i <= 10; i++){
+//     result = 2 * i
+//     // console.log(2 + ' x ' + i + ' = ' + result)
+//     console.log(`2 x ${i} = ${result}`)
+// }
+
+
+// calculator
+function calculator(operator){
+    let n1 = parseFloat(document.getElementById('num1').value)
+    let n2 = parseFloat(document.getElementById('num2').value)
+    let result = 0
+
+    if(n1 === "" || n2 === ""){
+        alert('Inputs should not be empty')
+    }
+    if(isNaN(n1) || isNaN(n2)){
+        alert('Only numbers are allowed')
+    }
+
+    // operations 
+    if(operator === '+') result = n1 + n2
+    else if(operator === '-') result = n1 - n2
+    else if(operator === 'x') result = n1 * n2
+    else if(operator === '/') result = n1 / n2
+    else "Operator not valid"
+
+    document.getElementById('result').innerText = result
+
+        
+}
+
+// console.log(isNaN(5))
